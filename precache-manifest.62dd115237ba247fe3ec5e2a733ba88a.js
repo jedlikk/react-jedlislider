20a8af8c746fb7d0356fcb8ff31af572c41b719f
Bartlomiej Jedlikowski <jedlikowskib@gmail.com>
2021-03-29 15:48:40 +0200

    Updates

---
+++ precache-manifest.62dd115237ba247fe3ec5e2a733ba88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e52b0f6dbaab1d891489d0c0caccbe8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47caa5966651cae1b82c",
+    "url": "./static/css/main.207b1a2e.chunk.css"
+  },
+  {
+    "revision": "b0fd34d2a699bc06706a",
+    "url": "./static/js/2.6c6dc249.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6c6dc249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47caa5966651cae1b82c",
+    "url": "./static/js/main.8c7e2c69.chunk.js"
+  },
+  {
+    "revision": "1b997a559b6b9c359f54",
+    "url": "./static/js/runtime-main.bf0ea9df.js"
+  }
+]);
